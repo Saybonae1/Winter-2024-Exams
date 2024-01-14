505974060aa6514ea4ba6be7a3c7adeadcdd15c6
Kurama Latespring <58215326+Saybonae1@users.noreply.github.com>
2024-01-14 22:58:11 +0000

    Update take.js

--- Tasks/take.js
+++ Tasks/take.js
@@ -1,12 +1,23 @@
 // Copy all listed keys from dictionary
 
-// Remove
-const copyK = (DX, ...xor) => {
-  const objK = Object.keys(DX);
-  objK.forEach((key) => {
-    if (!keys.includes(key)) delete DX [key];
-  });
-  return DX;
+// look and fix
+const copyK = (DX, ...keys) =>
+{
+
+  const resDX = {};
+  const objKeys = Object.keys(DX);
+  for (const key of objKeys)
+  {
+
+    if (keys.includes(key))
+    {
+
+      resDX[key] = DX[key];
+    }
+
+  }
+  return resDX;
+
 };
 
 module.exports = copyK;
