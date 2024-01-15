1fd1270ce8524d3bc8ffa1585509904b92f2cc1d
Kurama Latespring <58215326+Saybonae1@users.noreply.github.com>
2024-01-15 09:40:36 +0000

    Update sample.js

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,8 +1,8 @@
 // Get one random element from an array
 // name and fix
 const getRandElement = (arr) => {
-  arr = arr[Math.floor(Math.random() * arr.length)];
-  return arr;
+  const randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
 };
 
 module.exports = getRandlement;
