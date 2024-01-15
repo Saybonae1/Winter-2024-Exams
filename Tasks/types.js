b7e0108bf9a79c5095c7a23bbb70e938e6614d70
Kurama Latespring <58215326+Saybonae1@users.noreply.github.com>
2024-01-15 08:56:35 +0000

    Update types.js

--- Tasks/types.js
+++ Tasks/types.js
@@ -2,10 +2,19 @@
 
 // remove
 
+const types_ = function (s) {
+  const types_ = {
+    number: 0,
+    string: 0,
+    boolean: 0,
+  };
+  for (const i of s) {
     const t = typeof i;
     types_[t]++;
   }
+  s.push('string');
   return types_;
+  s.length;
 };
 
 module.exports = types_;
